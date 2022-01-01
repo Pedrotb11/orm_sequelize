3cb0f516fc0a831a0b93d5f5259723f9dca1a2c6
ptbelo <ptbelo@hotmail.com>
2022-01-01 14:31:15 -0300

    exportando o modulo PessoaController

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -1,7 +1,4 @@
-// Primeiro, antes de começarmos a trabalhar, devemos lembrar de importar tudo o que precisamos para esse arquivo. Se o controlador vai se conectar com o modelo, precisamos passar para ele onde estão nossos modelos. Então vou criar essa const chamada database e dentro dela usarei o require para puxar os arquivos de modelo. O código fica: const database = require(‘../models’). O /models fica entre aspas por ser uma string.
-
-const data = require('../models'); // Quando escrevemos dessa forma, por padrão, o JavaScript já vai dentro da pasta “modelos” e procura um arquivo chamado index, então não precisamos escrever models/index.js, só models ele já acha.
-
+const data = require('../models');
 class PessoaController {
     static async pegaTodasAsPessoas(req, res) {
         try {
@@ -12,3 +9,5 @@ class PessoaController {
         }
     }
 }
+
+module.exports = PessoaController;
