8b8fcb0b53cc4703ad202057fd2c434edf32c0ee
ptbelo <ptbelo@hotmail.com>
2022-01-02 15:03:34 -0300

    Definindo rota de pegar apenas uma pessoa

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -4,5 +4,6 @@ const PessoasController = require('../controllers/PessoaController')
 const router = Router()
 
 router.get('/pessoas', PessoasController.pegaTodasAsPessoas)
+router.get('/pessoas/:id', PessoasController.pegaUmaPessoa)
 
 module.exports = router
