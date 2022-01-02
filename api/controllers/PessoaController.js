417bf6be30477033c346faf202295508d37ef980
ptbelo <ptbelo@hotmail.com>
2022-01-02 13:59:04 -0300

    Definindo a primeira rota(pessoasRoute) erro corrigido

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -1,11 +1,11 @@
-const data = require('../models');
+const database = require('../models');
 class PessoaController {
     static async pegaTodasAsPessoas(req, res) {
         try {
             const todasAsPessoas = await database.Pessoas.findAll()
             return res.status(200).json(todasAsPessoas)
         } catch (error) {
-            return res.status(200).json(error.message)
+            return res.status(500).json(error.message)
         }
     }
 }
