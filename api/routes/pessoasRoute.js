b8e2f3b14e487d4ca7af7d594bc368f382f3504e
ptbelo <ptbelo@hotmail.com>
2022-01-02 21:07:12 -0300

    Definindo rota de atualizar e deletar

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -6,5 +6,7 @@ const router = Router()
 router.get('/pessoas', PessoasController.pegaTodasAsPessoas)
 router.get('/pessoas/:id', PessoasController.pegaUmaPessoa)
 router.post('/pessoas', PessoasController.criaPessoa)
+router.put('/pessoas/:id', PessoasController.atualizaPessoa)
+router.delete('/pessoas/:id', PessoasController.apagaPessoa)
 
 module.exports = router
