5ac77f9fef4c076a6702739347e014141a18679e
ptbelo <ptbelo@hotmail.com>
2022-01-05 12:32:30 -0300

    Finalizando os servicos

--- api/services/PessoasServices.js
+++ api/services/PessoasServices.js
@@ -8,22 +8,26 @@ class PessoasServices extends Services {
     }
 
     async pegaRegistrosAtivos(where = {}) {
-        return database[this.nomeDoModelo].findAll({ where: {...where } }) // Agora findAll vai funcionar tanto se where não tiver nada, for um objeto vazio, vai funcionar da mesma forma que se não estivesse passando parâmetro nenhum, mas agora posso utilizar esse método para passar parâmetros dentro do findAll como where, coluna x tem tal valor, etc.
+        return database[this.nomeDoModelo].findAll({ where: {...where } })
     }
 
     async pegaTodosOsRegistros(where = {}) {
         return database[this.nomeDoModelo]
             .scope('todos')
-            .findAll({ where: {...where } }) //Se recebeu where, qualquer tipo de informação de dado para ele selecionar ele vai receber, senão vai funcionar findAll com where vazio.
+            .findAll({ where: {...where } })
     }
 
-    async cancelaPessoaEMatriculas(estudanteId) { // O que ele tem que receber? A mesma coisa que por enquanto o método cancelaPessoa está recebendo no controlador, que é só estudanteId, porque através do estudanteId fazemos todas as outras alterações.O que ele vai precisar retornar? Ele vai retornar a transação inteira e dentro dela o que temos que fazer. Vou copiar a linha onde está escrito a transação:
-        return database.sequelize.transaction(async transacao => { //vamos substituir esses updates pelos métodos que criamos dentro de services.js, atualizaRegistro e atualizaRegistros.
-            await super.atualizaRegistro({ ativo: false }, estudanteId, {
-                transaction: transacao
-            })
+    async cancelaPessoaEMatriculas(estudanteId) {
+        return database.sequelize.transaction(async transacao => {
+            await super.atualizaRegistro({ ativo: false }, estudanteId, { transaction: transacao })
             await this.matriculas.atualizaRegistros({ status: 'cancelado' }, { estudante_id: estudanteId }, { transaction: transacao })
         })
+    }
+
+    async pegaMatriculasPorEstudante(where = {}) {
+        const matriculas = await database[this.nomeDoModelo]
+            .findOne({ where: {...where } })
+        return matriculas.getAulasMatriculadas()
     }
 }
 
--- api/services/Services.js
+++ api/services/Services.js
@@ -1,5 +1,3 @@
-// É essa a responsabilidade de conectar com a database e processar esses dados que vai receber, enviar, etc, que vamos tirar dos controladores e passar para camada services, que vai ficar entre o controlador e o modelo, digamos assim.
-
 const database = require('../models')
 
 class Services {
@@ -7,30 +5,44 @@ class Services {
         this.nomeDoModelo = nomeDoModelo
     }
 
-    async pegaTodosOsRegistros() {
-        return database[this.nomeDoModelo].findAll()
+    async pegaTodosOsRegistros(where = {}) {
+        return database[this.nomeDoModelo].findAll({ where: {...where } })
     }
 
-    async pegaUmRegistro(id) {
-
+    async pegaUmRegistro(where = {}) {
+        return database[this.nomeDoModelo].findOne({ where: {...where } })
     }
 
     async criaRegistro(dados) {
-
+        return database[this.nomeDoModelo].create(dados)
     }
 
-    async atualizaRegistro(dadosAtualizados, id, transacao = {}) { // Vamos conseguir usar atualizaRegistro com ou sem transação, porque se não tiver nenhuma transação o sequelize não vai passar isso para frente,
+    async atualizaRegistro(dadosAtualizados, id, transacao = {}) {
         return database[this.nomeDoModelo]
-            .update(dadosAtualizados, { where: { id: id } }, transacao) // e se tiver ele vai passar com as informações que ele vai receber do call-back de sequelize.transaction.
+            .update(dadosAtualizados, { where: { id: id } }, transacao)
     }
 
-    async atualizaRegistros(dadosAtualizados, where, transacao = {}) { //esse método atualizaRegistro que atualiza um registro baseado no id, vamos precisar também de um um pouco mais específico, que receba um where e aí faça alteração de todas as linhas onde where se aplica.
-            return database[this.nomeDoModelo]
-                .update(dadosAtualizados, { where: {...where } }, transacao)
-        } // ao invés de um id único ele vai receber um where. Então vou passar o where como parâmetro. A única alteração que vamos fazer no update é que ao invés do where receber um id:id ele vai receber um spread de where. Como vamos usar esse método? Vamos passar como parâmetro do where o objeto onde ele vai montar as condições do where onde ele vai procurar os registros nas tabelas e fazer as atualizações.
+    async atualizaRegistros(dadosAtualizados, where, transacao = {}) {
+        return database[this.nomeDoModelo]
+            .update(dadosAtualizados, { where: {...where } }, transacao)
+    }
 
     async apagaRegistro(id) {
+        return database[this.nomeDoModelo].destroy({ where: { id: id } })
+    }
 
+    async restauraRegistro(id) {
+        return database[this.nomeDoModelo].restore({ where: { id: id } })
+    }
+
+    async consultaRegistroApagado(id) {
+        return database[this.nomeDoModelo]
+            .findOne({ paranoid: false, where: { id: Number(id) } })
+    }
+
+    async encontraEContaRegistros(where = {}, agregadores) {
+        return database[this.nomeDoModelo]
+            .findAndCountAll({ where: {...where }, ...agregadores })
     }
 
 }
