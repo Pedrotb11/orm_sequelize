ea85a5c4bb1c4277151492362c88266d29960910
ptbelo <ptbelo@hotmail.com>
2022-01-04 23:40:25 -0300

    Operacoes em dois modelos (ultimo pedido da lista)

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -201,6 +201,20 @@ class PessoaController {
         }
     }
 
+    static async cancelaPessoa(req, res) {
+        const { estudanteId } = req.params
+        try {
+            await database.Pessoas
+                .update({ ativo: false }, { where: { id: Number(estudanteId) } }) //O que o método update tem que receber por parâmetro para funcionar? Primeiro ele tem que receber as informações que vão ser atualizadas, que vão ser updated, e depois ele tem que receber um where para saber onde tem que atualizar.
+            await database.Matriculas
+                .update({ status: 'cancelado' }, { where: { estudante_id: Number(estudanteId) } })
+
+            return res.status(200).json({ message: `matriculas ref. estudante ${estudanteId} canceladas` })
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
 }
 
 module.exports = PessoaController
